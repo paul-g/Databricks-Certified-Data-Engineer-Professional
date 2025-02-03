45eb2b2fd47a489bb2f3ee4db63a94fa7467df70
Paul Grigoras <paul.grigoras90@gmail.com>
2025-02-03 12:12:58 +0000

    Stub out chapter 2

--- 2 - Data Modeling/2.1 - Multiplex Bronze.py
+++ 2 - Data Modeling/2.1 - Multiplex Bronze.py
@@ -21,27 +21,7 @@ display(df_raw)
 
 # COMMAND ----------
 
-from pyspark.sql import functions as F
-
-def process_bronze():
-  
-    schema = "key BINARY, value BINARY, topic STRING, partition LONG, offset LONG, timestamp LONG"
-
-    query = (spark.readStream
-                        .format("cloudFiles")
-                        .option("cloudFiles.format", "json")
-                        .schema(schema)
-                        .load(f"{dataset_bookstore}/kafka-raw")
-                        .withColumn("timestamp", (F.col("timestamp")/1000).cast("timestamp"))  
-                        .withColumn("year_month", F.date_format("timestamp", "yyyy-MM"))
-                  .writeStream
-                      .option("checkpointLocation", "dbfs:/mnt/demo_pro/checkpoints/bronze")
-                      .option("mergeSchema", True)
-                      .partitionBy("topic", "year_month")
-                      .trigger(availableNow=True)
-                      .table("bronze"))
-    
-    query.awaitTermination()
+# write a pyspark function to load all available data from kafka-raw into a table named bronze
 
 # COMMAND ----------
 
--- 2 - Data Modeling/2.5 - Type 2 SCD.py
+++ 2 - Data Modeling/2.5 - Type 2 SCD.py
@@ -15,52 +15,12 @@
 # MAGIC -- NOTE: This cell contains the SQL query for your reference, and won't work if run directly.
 # MAGIC -- The query is used below in the type2_upsert() function as part of the foreachBatch call.
 # MAGIC
-# MAGIC MERGE INTO books_silver
-# MAGIC USING (
-# MAGIC     SELECT updates.book_id as merge_key, updates.*
-# MAGIC     FROM updates
-# MAGIC
-# MAGIC     UNION ALL
-# MAGIC
-# MAGIC     SELECT NULL as merge_key, updates.*
-# MAGIC     FROM updates
-# MAGIC     JOIN books_silver ON updates.book_id = books_silver.book_id
-# MAGIC     WHERE books_silver.current = true AND updates.price <> books_silver.price
-# MAGIC   ) staged_updates
-# MAGIC ON books_silver.book_id = merge_key 
-# MAGIC WHEN MATCHED AND books_silver.current = true AND books_silver.price <> staged_updates.price THEN
-# MAGIC   UPDATE SET current = false, end_date = staged_updates.updated
-# MAGIC WHEN NOT MATCHED THEN
-# MAGIC   INSERT (book_id, title, author, price, current, effective_date, end_date)
-# MAGIC   VALUES (staged_updates.book_id, staged_updates.title, staged_updates.author, staged_updates.price, true, staged_updates.updated, NULL)
+# MAGIC -- write a sql query to create the streaming books table in silver layer, with SCD type 2 support
 
 # COMMAND ----------
 
 def type2_upsert(microBatchDF, batch):
-    microBatchDF.createOrReplaceTempView("updates")
-    
-    sql_query = """
-        MERGE INTO books_silver
-        USING (
-            SELECT updates.book_id as merge_key, updates.*
-            FROM updates
-
-            UNION ALL
-
-            SELECT NULL as merge_key, updates.*
-            FROM updates
-            JOIN books_silver ON updates.book_id = books_silver.book_id
-            WHERE books_silver.current = true AND updates.price <> books_silver.price
-          ) staged_updates
-        ON books_silver.book_id = merge_key 
-        WHEN MATCHED AND books_silver.current = true AND books_silver.price <> staged_updates.price THEN
-          UPDATE SET current = false, end_date = staged_updates.updated
-        WHEN NOT MATCHED THEN
-          INSERT (book_id, title, author, price, current, effective_date, end_date)
-          VALUES (staged_updates.book_id, staged_updates.title, staged_updates.author, staged_updates.price, true, staged_updates.updated, NULL)
-    """
-    
-    microBatchDF.sparkSession.sql(sql_query)
+  # write a functino to apply type2 upsert
 
 # COMMAND ----------
 
@@ -73,19 +33,7 @@ def type2_upsert(microBatchDF, batch):
 def process_books():
     schema = "book_id STRING, title STRING, author STRING, price DOUBLE, updated TIMESTAMP"
  
-    query = (spark.readStream
-                    .table("bronze")
-                    .filter("topic = 'books'")
-                    .select(F.from_json(F.col("value").cast("string"), schema).alias("v"))
-                    .select("v.*")
-                 .writeStream
-                    .foreachBatch(type2_upsert)
-                    .option("checkpointLocation", "dbfs:/mnt/demo_pro/checkpoints/books_silver")
-                    .trigger(availableNow=True)
-                    .start()
-            )
-    
-    query.awaitTermination()
+    # write a pyspark query to load orders from bronze to silver, using type2 SCD
     
 process_books()
 
@@ -116,10 +64,8 @@ display(books_df)
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE OR REPLACE TABLE current_books
-# MAGIC AS SELECT book_id, title, author, price
-# MAGIC    FROM books_silver
-# MAGIC    WHERE current IS TRUE
+# MAGIC -- write a SQL query to create a view of the current books, current_books (book_id, title, author, price) from books_silver
+# MAGIC
 
 # COMMAND ----------
 
